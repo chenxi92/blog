df548a19734dc1ce66eeb7c9eddfd0eebd06fbf6
peak <peak@jiemogame.com>
2023-10-12 18:46:25 +0800

    change remote branch name

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'update'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chenxi92/blog.git master:gh-pages
+git push -f git@github.com:chenxi92/blog.git main:gh-pages
 
 cd -
